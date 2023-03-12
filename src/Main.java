98c065119ec3c8447312d067e31a1adf9fda33c4
Ekaterina Pavlova <sone4ka1414@gmail.com>
2023-03-12 19:51:16 +0300

    bonus calculation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int initial = 250;
+        int paid = 1100;
+        int bonus;
+
+        if (paid >= 1000) {
+            bonus = paid / 100;
+        } else {
+            bonus = 0;
+        }
+        int total = initial + paid + bonus;
+        System.out.println("Итоговый счет: " + total);
+        System.out.println("Бонусы: " + bonus);
     }
 }
